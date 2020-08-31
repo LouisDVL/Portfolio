af3e72fe7665ff71e2450a89ca9bb9f345ea914a
LouisDVL <louis.lising@outlook.com>
2020-09-01 09:48:38 +1200

    Updates

---
+++ precache-manifest.a4b2f9c859d5d192308cbb3bbc2fce0f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cafc899f52c70b32c956",
+    "url": "/Portfolio/static/css/main.11b5f500.chunk.css"
+  },
+  {
+    "revision": "cafc899f52c70b32c956",
+    "url": "/Portfolio/static/js/main.f373977d.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "455474f4fb5d0c5ac04e",
+    "url": "/Portfolio/static/js/2.2c5a1b73.chunk.js"
+  },
+  {
+    "revision": "c4f46c77e8afd34ddd3440db2af5f3f3",
+    "url": "/Portfolio/static/media/shop.c4f46c77.PNG"
+  },
+  {
+    "revision": "474d62943c34506451e234c777e4633c",
+    "url": "/Portfolio/static/media/login.474d6294.PNG"
+  },
+  {
+    "revision": "d0762c98e654796ee6aff5aa1ff38cb7",
+    "url": "/Portfolio/static/media/home.d0762c98.PNG"
+  },
+  {
+    "revision": "9d000faf1aaf96730d5f904763509bef",
+    "url": "/Portfolio/static/media/profile.9d000faf.PNG"
+  },
+  {
+    "revision": "96e978440fbd5bfbbd09438dad38e584",
+    "url": "/Portfolio/static/media/shopAdd.96e97844.PNG"
+  },
+  {
+    "revision": "dd9227502ee1af182de80e1b2f4f2095",
+    "url": "/Portfolio/index.html"
+  }
+];
