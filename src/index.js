51773cdebd20005fa783fb4e9529bbd024b434f4
LouisDVL <louis.lising@outlook.com>
2020-09-01 13:01:27 +1200

    route for home page fixed

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
         <BrowserRouter>
             <Switch>
                 <ScrollToTop>
-                    <Route path={process.env.PUBLIC_URL + '/'} component={App} />
+                    <Route exact path={process.env.PUBLIC_URL + '/'} component={App} />
                     <Route path={process.env.PUBLIC_URL + '/RCK'} component={RckPage} />
                 </ScrollToTop>
             </Switch>
